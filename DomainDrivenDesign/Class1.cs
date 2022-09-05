311423148f0ea2923e8263818000033782918773
S.Mousavi <mahsa.msvi1994@gmail.com>
2022-09-05 17:38:43 +0430

    Create Customer project - Add class library

---
+++ DomainDrivenDesign/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainDrivenDesign
+{
+    public class Class1
+    {
+    }
+}
